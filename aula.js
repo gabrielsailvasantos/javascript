21be5e7efa8e62d574d5e32bf447ac67b71bdf48
Gabriel da Silva Santos <30303646+gabrielsailvasantos@users.noreply.github.com>
2022-05-23 18:41:21 -0300

    aulão de JS

---
+++ aula.js
@@ -0,0 +1,138 @@
+// let idade = 5;
+// console.log(idade);
+// let altura = 180;
+// console.log(altura);
+
+// let nomeCompleto = 'gabriel da silva santos';
+// console.log(nomeCompleto);
+// let idade = 26;
+// console.log(idade);
+// let conjugue = 'poliana eslane';
+// console.log(conjugue);
+// let idadeConjugue = 28;
+// console.log(idadeConjugue)
+
+// let nome = 'Gabriel'; // String literal
+// let idade = 25; // number literal
+// let estaAprovado = true; // boolean
+// let sobrenome = undefined; // Undefined
+// let corSelecionada = null // redefinir valor
+
+
+
+// let dadosPessoa = {
+//     nome: 'Gabriel', 
+//     idade: '26',
+//     estaAprovado: true, 
+//     sobrenome: 'Silva Santos',
+//     casado: true,
+//     filhos: true,
+//     ondeTrabalha: 'Construção'
+
+// }
+
+// console.log(dadosPessoa)
+
+
+// let familia = [true, 45, 'Gabriel', 47];
+// console.log(familia[0], familia[2]);
+// let nomeDoMeuAmo = ['Poliana', 28, 'escorpiana'];
+
+// console.log(nomeDoMeuAmo[0]);
+
+
+// Funções
+
+// let corSite = 'azul';
+// function trocaCor(cor='', tonalidade='normal'){
+//     corSite = cor + ' ' + tonalidade;
+// };
+
+// console.log(corSite);
+// trocaCor('laranja');
+// console.log(corSite);
+
+
+// let soma = 0;
+// function somar(a=0, b=0){
+//     soma = a + b;
+// };
+
+// somar(9, 5);
+// console.log(soma);
+
+
+// // funçoes com retorno
+
+// function multiplicaPorDois(valor){
+//     return valor * 2;
+// };
+
+// let resultado = multiplicaPorDois(5);
+// console.log(resultado)
+
+
+
+// // operador ternario
+// let pontos = 500;
+// resul = '';
+// let tipo = pontos > 100 ? resul = 'Cliente Premium' : resul = 'Cliente Comum' ;
+// console.log(tipo);
+// console.log(resul);
+
+
+// //   IF ELSE
+
+// let hora = 22;
+
+// if (hora > 6 && hora < 12){
+//     console.log('Bom dia');
+// }
+// else if (hora > 12 && hora < 18){
+//     console.log('Boa tarde');
+// }
+// else {
+// console.log('Boa noite')
+// };
+
+
+
+// //  Switch...Case
+
+// let permissao; // comum, gerente, diretor, cliente, visitante
+// permissao = 'cliente'
+
+// switch(permissao){
+//     case 'comum' :
+//         console.log('Usuario comum');
+//         break;
+    
+//     case 'gerente' :
+//         console.log('Usuario gerente');
+//         break;
+
+//     case 'diretor' :
+//         console.log('Usuario Diretor');
+//         break;
+
+//     case 'cliente' :
+//         console.log('Usuario Cliente')
+//         break;
+
+//     case 'visitante' :
+//         console.log('Usuario visitante');
+//         break;
+
+//     default:
+//         console.log('Usuario nao reconhecido!');
+
+// }
+
+
+//  1. FOR
+
+for (let i = 0; i < 5; i++){
+    console.log('Estou aprendendo!', i)
+}
+
+------1:20-----
